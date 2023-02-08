f361af8413b4022c3ee38c2078557658d6489617
PradeeptheDeveloper <pradeepthedeveloper@gmail.com>
2023-02-08 15:24:27 +0530

    initial commit

---
+++ lib/views/homepage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/product_controller.dart';
+import 'product_tile.dart';
+
+class HomePage extends StatelessWidget {
+  final ProductController productController = Get.put(ProductController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Learn with pradeep',
+                    style: TextStyle(
+                        fontFamily: 'avenir',
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(Icons.view_list_rounded), onPressed: () {}),
+                IconButton(icon: Icon(Icons.grid_view), onPressed: () {}),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Obx(() {
+              if (productController.isLoading.value)
+                return Center(child: CircularProgressIndicator());
+              else
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      childAspectRatio: 1 / 1.5),
+                  itemCount: productController.productList.length,
+                  itemBuilder: (context, i) =>
+                      ProductTile(productController.productList[i]),
+                );
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
